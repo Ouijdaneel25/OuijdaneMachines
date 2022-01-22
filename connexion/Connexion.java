b13e2a72ebe0b0298122e97618bd7a434798b81c
Ouijdaneel25 <60515139+Ouijdaneel25@users.noreply.github.com>
2022-01-22 16:57:31 +0100

    lm
    l

--- connexion/Connexion.java
+++ connexion/Connexion.java
@@ -6,36 +6,46 @@ import java.sql.SQLException;
 
 public class Connexion {
 
-    private static String login = "qmcdzpctqdiweq";
-    private static String password = "ad2aa790656390d317dd2c3fd296ffcbf711d786d3b8ce1d93108c023608c000";
-    private static String url = "jdbc:mysql://ec2-3-222-49-168.compute-1.amazonaws.com/dcifuoonpgeinc?sslmode=require";
     private Connection connection = null;
-    private static Connexion instane;
+	private static Connexion instane;
 
-    private Connexion() {
-       try {
-					Class.forName("org.postgresql.Driver").newInstance();
-				} catch (InstantiationException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (IllegalAccessException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (ClassNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-    }
+	private Connexion() {
 
-    public Connection getConnection() {
-        return connection;
-    }
+	}
 
-    public static Connexion getInstane() {
-        if (instane == null) {
-            instane = new Connexion();
-        }
-        return instane;
-    }
+	public Connection getConnection() {
+		try {
+			Class.forName("org.postgresql.Driver").newInstance();
+		} catch (InstantiationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IllegalAccessException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		String username = "qmcdzpctqdiweq";
+		String password = "ad2aa790656390d317dd2c3fd296ffcbf711d786d3b8ce1d93108c023608c000";
+		String dbUrl = "jdbc:postgresql://" + "ec2-3-222-49-168.compute-1.amazonaws.com:5432"
+				+ "/dcifuoonpgeinc?sslmode=require";
+		try {
+			return DriverManager.getConnection(dbUrl, username, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return connection;
+	}
+
+	public static Connexion getInstane() {
+		if (instane == null) {
+			instane = new Connexion();
+		}
+		return instane;
+	}
 
 }
