0a928a38b08b6d6053340a7b30c9705c27c3ac3c
ouijdane <ouijdaneelazri25@gmail.com>
2022-01-22 16:23:08 +0100

    ouijdane

--- src/main/java/connexion/Connexion.java
+++ src/main/java/connexion/Connexion.java
@@ -6,33 +6,47 @@ import java.sql.SQLException;
 
 public class Connexion {
 
-    private static String login = "qmcdzpctqdiweq";
-    private static String password = "ad2aa790656390d317dd2c3fd296ffcbf711d786d3b8ce1d93108c023608c000";
-    private static String url = "jdbc:postgres://ec2-3-222-49-168.compute-1.amazonaws.com/dcifuoonpgeinc?sslmode=require";
-    private Connection connection = null;
-    private static Connexion instane;
+	private static String login = "qmcdzpctqdiweq";
+	private static String password = "ad2aa790656390d317dd2c3fd296ffcbf711d786d3b8ce1d93108c023608c000";
+	private static String url = "jdbc:postgres://ec2-3-222-49-168.compute-1.amazonaws.com/dcifuoonpgeinc?sslmode=require";
+	private Connection connection = null;
+	private static Connexion instane;
 
-    private Connexion() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(url, login, password);
-        } catch (ClassNotFoundException e) {
-            System.out.println("Driver introvable");
-        } catch (SQLException e) {
-            System.out.println("Connexion errror");
-            System.out.println(e.getMessage());
-        }
-    }
+	private Connexion() {
+		try {
+			Class.forName("org.postgresql.Driver").newInstance();
+		} catch (InstantiationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IllegalAccessException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+//		try {
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+//			connection = DriverManager.getConnection(url, login, password);
+//		} catch (ClassNotFoundException e) {
+//			System.out.println("Driver introvable");
+//		} catch (SQLException e) {
+//			System.out.println("Connexion errror");
+//			System.out.println(e.getMessage());
+//		}
+	}
 
-    public Connection getConnection() {
-        return connection;
-    }
+	
 
-    public static Connexion getInstane() {
-        if (instane == null) {
-            instane = new Connexion();
-        }
-        return instane;
-    }
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public static Connexion getInstane() {
+		if (instane == null) {
+			instane = new Connexion();
+		}
+		return instane;
+	}
 
 }
